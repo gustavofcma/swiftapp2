77bea2c363d017f5907111bf6344ce9ecd903735
Gustavo de Almeida <gustavofcma@gmail.com>
2016-09-15 00:16:38 -0300

    Initial Commit

---
+++ Swift App 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift App 2
+//
+//  Created by Gustavo de Almeida on 15/09/16.
+//  Copyright © 2016 Gustavo de Almeida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
